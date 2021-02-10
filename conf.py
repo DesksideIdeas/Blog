ffa149575b690e3abe8ea80a104423aae490f831
Thomas Xie <68626438+DesksideIdeas@users.noreply.github.com>
2021-02-10 14:16:50 +0800

    Update conf.py

--- conf.py
+++ conf.py
@@ -21,7 +21,7 @@ enable_jsdelivr = {
 # 站点设置
 site_name = "桌沿奇思"
 site_logo = "${static_prefix}眼镜证件照.jpg"
-site_build_date = "2020-7-1T09:00+08:00"
+site_build_date = "2020-07-01T09:00+08:00"
 author = "谢卓彦"
 email = "hi@imalan.cn"
 author_homepage = "https://www.imalan.cn"
